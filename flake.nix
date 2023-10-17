7438d8911b31bea4c3b2dd548723e187d7d0e486
Florian Walter <nairolf.retlaw@gmail.com>
2023-10-17 12:28:33 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "project-euler";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            ghc
+            cabal-install
+            haskell-language-server
+          ];
+        };
+      });
+    };
+}
